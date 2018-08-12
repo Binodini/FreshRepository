92aa2b091a55381c14fbe6f28fb6a94ddd67491a
binodini.sahoo <binodini.sahoo@GISBIZ-LTP-85.gisbizdev.GISBIZ-CHENNAI.COM>
2018-08-12 11:28:45 +0530

    fresh move

---
+++ DevOps/src/main/java/DevOpsConcept/DevOps/App.java
@@ -0,0 +1,13 @@
+package DevOpsConcept.DevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
